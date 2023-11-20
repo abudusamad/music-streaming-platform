eafb4e07634f885cf758f7f4acd48fd9a3dfd0fa
abudusamad <abudusamed@gmail.com>
2023-11-20 18:41:36 +0000

    making my userProvider to wrap the code for the authenicate

---
+++ providers/UserProvider.tsx
@@ -0,0 +1,11 @@
+import { MyUserContextProvider } from "@/hooks/useUser";
+
+interface UserProviderProps {
+	children: React.ReactNode;
+}
+
+const UserProvider = ({ children }: UserProviderProps) => {
+	return <MyUserContextProvider>{children}</MyUserContextProvider>;
+};
+
+export default UserProvider;
