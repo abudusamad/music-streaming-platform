3cabff65191f2d5495854dbd8c065fbe058e6854
abudusamad <abudusamed@gmail.com>
2023-11-20 17:16:32 +0000

    making the supase client for the pages

---
+++ providers/SupabaseProivder.tsx
@@ -0,0 +1,22 @@
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useState } from "react";
+
+import { Database } from "@/types_db";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+
+interface SupabaseProviderProps {
+	children: React.ReactNode;
+}
+
+const SupabaseProvider = ({ children }: SupabaseProviderProps) => {
+	const [supabaseClient, setSupabaseClient] = useState(() =>
+		createClientComponentClient<Database>()
+	);
+	return (
+		<SessionContextProvider supabaseClient={supabaseClient}>
+			{children}
+		</SessionContextProvider>
+	);
+};
+
+export default SupabaseProvider;
