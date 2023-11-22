80ed1179af651bcb2271a273b4d998df65deba3f
abudusamad <abudusamed@gmail.com>
2023-11-22 01:04:20 +0000

    Finished with the fetching of the database from the database to the pages

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,28 +1,29 @@
 import useAuthModal from "@/hooks/useAuthModal";
+import useOnPlay from "@/hooks/useOnPlay";
 import useUploadModal from "@/hooks/useUploadModal";
 import { useUser } from "@/hooks/useUser";
 import { Song } from "@/types";
 import { AiOutlinePlus } from "react-icons/ai";
 import { GoArrowRight } from "react-icons/go";
 import { TbPlaylist } from "react-icons/tb";
+import MediaItem from "./MediaItem";
 
 interface LibraryProps {
-	songs: Song[]
+	songs: Song[];
 }
 
-const Library = ({
-	songs
-}:LibraryProps) => {
+const Library = ({ songs }: LibraryProps) => {
 	const { user } = useUser();
 	const authModal = useAuthModal();
 	const uploadModal = useUploadModal();
+
+	const onPlay = useOnPlay(songs);
 
 	const onClick = () => {
 		if (!user) {
 			authModal.onOpen();
 		}
 		return uploadModal.onOpen();
-		
 	};
 	return (
 		<div className="flex flex-col">
@@ -36,8 +37,10 @@ const Library = ({
 						Your Library
 					</p>
 				</div>
+
 				<div className="flex items-center justify-center gap-x-2">
-					<AiOutlinePlus onClick={onClick}
+					<AiOutlinePlus
+						onClick={onClick}
 						className="text-neutral-400 hover:text-white transition rounded-full bg-black bg-opacity-20 hover:bg-opacity-30 p-1"
 						size={30}
 					/>
@@ -46,6 +49,15 @@ const Library = ({
 						size={30}
 					/>
 				</div>
+			</div>
+			<div className="flex flex-col gap-y-2 mt-4 px-3">
+				{songs.map((song) => (
+					<MediaItem
+						key={song.id}
+						onClick={(id: string) => onPlay(id)}
+						data={song}
+					/>
+				))}
 			</div>
 		</div>
 	);
