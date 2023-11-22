d0a63bece5a1fd33f6ff148242f727a9c6b8bada
abudusamad <abudusamed@gmail.com>
2023-11-22 13:53:07 +0000

    Making the search  input from the search bar

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,20 +1,21 @@
+import getSongsByTitle from "@/actions/getSongsByTitle";
 import Header from "@/components/Header";
+import SearchInput from "@/components/SearchInput";
 
-interface SearchProps{
-    searchParams:{title:string}
+interface SearchProps {
+	searchParams: { title: string };
 }
 
-const Search = ({
-    searchParams
-}:SearchProps) => {
+const Search = async ({ searchParams }: SearchProps) => {
+	const songs = await getSongsByTitle(searchParams.title);
 	return (
-        <div>
-            <Header className="from-bg-neutral-900">
-                <div className="mb-2 flex flex-col gap-y-6">
-                    <h1 className="text-white text-3xl font-semibold">Search</h1>
-                    
+		<div>
+			<Header className="from-bg-neutral-900">
+				<div className="mb-2 flex flex-col gap-y-6">
+					<h1 className="text-white text-3xl font-semibold">Search</h1>
                 </div>
-            </Header>
+                <SearchInput/>
+			</Header>
 		</div>
 	);
 };
