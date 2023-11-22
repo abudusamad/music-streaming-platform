a1708de65e28f71a1d627d8c8531341dece38068
abudusamad <abudusamed@gmail.com>
2023-11-22 21:51:46 +0000

    making the liked songs pages

---
+++ app/liked/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import Header from "@/components/Header";
+import Image from "next/image";
+
+export const revalidate = 0;
+
+const LikedPage = () => {
+	return (
+		<div className="bg-neutral-900 rounded-lg w-full h-full overflow-hidden overflow-y-auto">
+			<Header>
+				<div className="flex flex-col md:flex-row items-center gap-x-5">
+					<div className="relative h-32 w-32 lg:h-44 lg:w-44">
+						<Image
+							fill
+							src="/images/liked.png"
+							alt="Liked songs"
+							className="object-cover"
+						/>
+                    </div>
+                    <div className="flex flex-col gap-y-2 mt-4 md:mt-0">
+                        <p className="hidden md:block font-semibold text-sm">
+                            Playlist
+                        </p>
+                        <h1 className="text-white text-4xl sm:text-5xl lg:text-7xl font-bold">
+                            Liked Songs
+                        </h1>
+                    </div>
+				</div>
+			</Header>
+		</div>
+	);
+};
+
+export default LikedPage;
