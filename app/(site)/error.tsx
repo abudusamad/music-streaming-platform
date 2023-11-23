a25c663dc52557245a6024e2f1644217dac744f3
abudusamad <abudusamed@gmail.com>
2023-11-23 21:42:28 +0000

    making error page when something went wrong as fecthign data

---
+++ app/(site)/error.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import Box from "@/components/Box";
+
+const Erro = () => {
+	return (
+		<Box className="flex items-center justify-center h-full">
+			<div className="text-neural-400">Something went wrong</div>
+		</Box>
+	);
+};
