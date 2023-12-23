6d0ca8a259696501a7b6a31c766944bad16dadd5
abudusamad <abudusamed@gmail.com>
2023-12-23 15:32:22 +0000

    Adding the stripe env to the path

--- lib/stripeClient.ts
+++ lib/stripeClient.ts
@@ -1,1 +1,10 @@
-import {loadStripe, Stripe} from '@stripe/stripe-js';
+import { loadStripe, Stripe } from "@stripe/stripe-js";
+
+let stripePromise: Promise<Stripe | null>;
+
+export const getStripe = () => {
+	if (!stripePromise) {
+		stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY!);
+	}
+	return stripePromise;
+};
