937d3c023fb889c9ce6022bcab88473f2661f651
abudusamad <abudusamed@gmail.com>
2023-12-31 18:01:22 +0000

    Making the profile pictures

---
+++ app/settings/page.tsx
@@ -0,0 +1,28 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+
+const SettingsPage = () => {
+	return (
+		<div
+			className="
+            flex
+             flex-col
+              min-h-screen 
+        bg-neutral-900
+        rounded-lg
+        h-full
+        w-full
+        overflow-hidden
+        overflow-y-auto
+        "
+		>
+			<Header className="from-bg-neutral-900">
+				<h1 className="text-white text-2xl font-semibold">Settings</h1>
+			</Header>
+
+			<Footer />
+		</div>
+	);
+};
+
+export default SettingsPage;
